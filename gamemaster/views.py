38a05ecc82397475c1292d5745fbec78f17623ce
Maria Lopez-Latorre <mentatzoe@gmail.com>
2015-02-04 16:31:55 +0100

    Basic Pyramid testing
    Playing around with views and routing in Pyramid, seeing differences
    with other Pylons implementations.

--- gamemaster/views.py
+++ gamemaster/views.py
@@ -1,6 +1,20 @@
 from pyramid.view import view_config
+import logging
 
-
+log = logging.getLogger(__name__)
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'GameMaster'}
+
+@view_config(route_name='home2', renderer='templates/mytemplate2.pt')
+def my_view2(request):
+    log.info('asdf')
+    log.info(request.matchdict)
+    return {'bar': request.matchdict['foo']}
+
+@view_config(route_name='home3', renderer='templates/foo.pt')
+def idk(request):
+    log.info("Something else")
+    log.info(request.matchdict)
+    yet_another_test = int(request.matchdict['foo2']) * 3
+    return {'foo': yet_another_test}
